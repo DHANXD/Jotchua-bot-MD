2444a0b17f431fd8a4db1c795d8689984bdc0f9c
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2023-11-08 15:22:48 -0300

    Update menu-animes.js

--- plugins/menu-animes.js
+++ plugins/menu-animes.js
@@ -18,15 +18,16 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
     const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
     const document = doc[Math.floor(Math.random() * doc.length)];
-    const str = `╭═══〘 ✯✯✯✯✯✯✯✯✯ 〙══╮
-║    ◉— *𝐓𝐡𝐞 𝐌𝐲𝐬𝐭𝐢𝐜 - 𝐁𝐨𝐭* —◉
-║≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡║
-║➤ *𝗛ola, ${taguser}*
-╰═══╡✯✯✯✯✯✯✯✯✯╞═══╯
+    const str =
+`╭⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊
+┃  ◉— *𝐣𝐨𝐭𝐜𝐡𝐮𝐚-𝐛𝐨𝐭-𝐌𝐃* —◉
+┃⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊
+┃➤ *𝗛ola, ${taguser}*
+╰⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊
 
-┏━━━━━━━━━━━━━━━━┓
+╭⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊
 ┃ *< MENU-ANIME />*
-┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡┃
+┃⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊
 ┣ ඬ⃟ 🧿 _${usedPrefix}lolivid_
 ┣ ඬ⃟ 🧿 _${usedPrefix}loli_
 ┣ ඬ⃟ 🧿 _${usedPrefix}ppcouple_
@@ -67,7 +68,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
 ┣ ඬ⃟ 🧿 _${usedPrefix}sasuke_
 ┣ ඬ⃟ 🧿 _${usedPrefix}sakura_
 ┣ ඬ⃟ 🧿 _${usedPrefix}cosplay_
-┗━━━━━━━━━━━━━━━━┛`.trim();
+╰⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊`.trim();
     if (m.isGroup) {
       // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
       const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
