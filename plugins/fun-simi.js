105256a746a2cd859d6ea1cc7d20d5124abe0a44
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2023-11-12 19:25:11 -0300

    Update fun-simi.js

--- plugins/fun-simi.js
+++ plugins/fun-simi.js
@@ -1,30 +1,21 @@
-import translate from '@vitalets/google-translate-api';
-import fetch from 'node-fetch';
-const handler = async (m, {text, command, args, usedPrefix}) => {
-  if (!text) throw `*[❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝚃𝙴𝚇𝚃𝙾 𝙿𝙰𝚁𝙰 𝙷𝙰𝙱𝙻𝙰𝚁 𝙲𝙾𝙽 𝚂𝙸𝙼𝚂𝙸𝙼𝙸 𝙾 𝙴𝙻 𝙱𝙾𝚃*\n\n*𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command} Hola bot*`;
-  try {
-    const api = await fetch('https://api.simsimi.net/v2/?text=' + text + '&lc=es');
-    const resSimi = await api.json();
-    m.reply(resSimi.success);
-  } catch {
-    try {
-      if (text.includes('Hola')) text = text.replace('Hola', 'Hello');
-      if (text.includes('hola')) text = text.replace('hola', 'Hello');
-      if (text.includes('HOLA')) text = text.replace('HOLA', 'HELLO');
-      const reis = await fetch('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=' + text);
-      const resu = await reis.json();
-      const nama = m.pushName || '1';
-      const api = await fetch('http://api.brainshop.ai/get?bid=153868&key=rcKonOgrUFmn5usX&uid=' + nama + '&msg=' + resu[0][0][0]);
-      const res = await api.json();
-      const reis2 = await fetch('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=es&dt=t&q=' + res.cnt);
-      const resu2 = await reis2.json();
-      m.reply(resu2[0][0][0]);
-    } catch {
-      throw `*[❗] 𝙴𝚁𝚁𝙾𝚁, 𝚅𝚄𝙴𝙻𝚅𝙴 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*`;
-    }
-  }
-};
-handler.help = ['simi', 'bot'].map((v) => v + ' <teks>');
-handler.tags = ['fun'];
-handler.command = /^((sim)?simi|bot|alexa|cortana)$/i;
-export default handler;
+import fetch from 'node-fetch'
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+
+ let lang = global.db.data.users[m.sender].language
+  if (!text) throw `✳️ ${mssg.notext}`
+  m.react('🗣️') 
+  try { 
+  //let res = await fetch(global.API('https://api.simsimi.net', '/v2/', { text: encodeURIComponent(m.text), lc: "es" }, ''))
+  let res = await fetch(`https://api.simsimi.net/v2/?text=${text}&lc=${lang}`)
+  let json = await res.json()
+  m.reply(json.success.replace('simsimi', `${botName}`).replace('Simsimi', `${botName}`).replace('sim simi', `${botName}`))
+} catch {
+  m.reply(`❎ Intenta de nuevo mas tarde La api de SimSimi se cayo`)
+}
+
+}
+handler.help = ['bot']
+handler.tags = ['fun']
+handler.command = ['bot', 'simi'] 
+
+export default handler
