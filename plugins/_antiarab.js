edcb10aa6602e091d3db6eef6f4ec24a3014c32b
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2023-11-14 19:13:52 -0300

    Update _antiarab.js

--- plugins/_antiarab.js
+++ plugins/_antiarab.js
@@ -8,19 +8,19 @@ handler.before = async function(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner
   const bot = global.db.data.settings[conn.user.jid] || {};
   if (isBotAdmin && chat.antiArab2 && !isAdmin && !isOwner && !isROwner && bot.restrict) {
     if (m.sender.startsWith('212' || '212')) {
-      m.reply(`*[❗] ᴇɴ ᴇsᴛᴇ ɢʀᴜᴘᴏ ɴᴏ sᴇ ᴘᴇʀᴍɪᴛᴇɴ ɴᴜᴍᴇʀᴏs ᴀʀᴀʙᴇs ᴏ ʀᴀʀᴏs, ᴘᴏʀ ʟᴏ ϙᴜᴇ sᴇ ᴛᴇ sᴀᴄᴀʀᴀ ᴅᴇʟ ɢʀᴜᴘᴏ*`);
+      m.reply(`*𝐞𝐧 𝐞𝐬𝐭𝐞 𝐠𝐫𝐮𝐩𝐨 𝐧𝐨 𝐬𝐞 𝐩𝐞𝐫𝐦𝐢𝐭𝐞𝐧 𝐧𝐮𝐦𝐞𝐫𝐨𝐬 𝐚𝐫𝐚𝐛𝐞𝐬 𝐨 𝐫𝐚𝐫𝐨𝐬 𝐩𝐨𝐫 𝐥𝐨 𝐪𝐮𝐞 𝐬𝐞 𝐭𝐞 𝐬𝐚𝐜𝐚𝐫𝐚 𝐝𝐞𝐥 𝐠𝐫𝐮𝐩𝐨*`);
       const responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
       if (responseb[0].status === '404') return;
     }
 
     if (m.sender.startsWith('265' || '265')) {
-      m.reply(`*[❗] ᴇɴ ᴇsᴛᴇ ɢʀᴜᴘᴏ ɴᴏ sᴇ ᴘᴇʀᴍɪᴛᴇɴ ɴᴜᴍᴇʀᴏs ᴀʀᴀʙᴇs ᴏ ʀᴀʀᴏs, ᴘᴏʀ ʟᴏ ϙᴜᴇ sᴇ ᴛᴇ sᴀᴄᴀʀᴀ ᴅᴇʟ ɢʀᴜᴘᴏ*`);
+      m.reply(`*𝐞𝐧 𝐞𝐬𝐭𝐞 𝐠𝐫𝐮𝐩𝐨 𝐧𝐨 𝐬𝐞 𝐩𝐞𝐫𝐦𝐢𝐭𝐞𝐧 𝐧𝐮𝐦𝐞𝐫𝐨𝐬 𝐚𝐫𝐚𝐛𝐞𝐬 𝐨 𝐫𝐚𝐫𝐨𝐬 𝐩𝐨𝐫 𝐥𝐨 𝐪𝐮𝐞 𝐬𝐞 𝐭𝐞 𝐬𝐚𝐜𝐚𝐫𝐚 𝐝𝐞𝐥 𝐠𝐫𝐮𝐩𝐨*`);
       const responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
       if (responseb[0].status === '404') return;
     }
 
     if (m.sender.startsWith('92' || '92')) {
-      m.reply(`*[❗] ᴇɴ ᴇsᴛᴇ ɢʀᴜᴘᴏ ɴᴏ sᴇ ᴘᴇʀᴍɪᴛᴇɴ ɴᴜᴍᴇʀᴏs ᴀʀᴀʙᴇs ᴏ ʀᴀʀᴏs, ᴘᴏʀ ʟᴏ ϙᴜᴇ sᴇ ᴛᴇ sᴀᴄᴀʀᴀ ᴅᴇʟ ɢʀᴜᴘᴏ*`);
+      m.reply(`*𝐞𝐧 𝐞𝐬𝐭𝐞 𝐠𝐫𝐮𝐩𝐨 𝐧𝐨 𝐬𝐞 𝐩𝐞𝐫𝐦𝐢𝐭𝐞𝐧 𝐧𝐮𝐦𝐞𝐫𝐨𝐬 𝐚𝐫𝐚𝐛𝐞𝐬 𝐨 𝐫𝐚𝐫𝐨𝐬 𝐩𝐨𝐫 𝐥𝐨 𝐪𝐮𝐞 𝐬𝐞 𝐭𝐞 𝐬𝐚𝐜𝐚𝐫𝐚 𝐝𝐞𝐥 𝐠𝐫𝐮𝐩𝐨*`);
       const responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
       if (responseb[0].status === '404') return;
     }
