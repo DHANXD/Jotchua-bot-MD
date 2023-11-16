fd41c4adddb81672e83d26c9265afbefb33900de
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2023-11-16 18:47:08 -0300

    Update adult-comandos+18.js

--- plugins/adult-comandos+18.js
+++ plugins/adult-comandos+18.js
@@ -22,7 +22,7 @@ const handler = async (m, {command, conn}) => {
   }
 
   if (command == 'nsfwbdsm') {
-    const res = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/nsfwbdsm.json`)).data;
+    const res = (await axios.get(`https://raw.githubusercontent.com/AleXD0009/Jotchua-bot-MD/master/src/JSON/nsfwbdsm.json`)).data;
     const haha = await res[Math.floor(res.length * Math.random())];
     conn.sendMessage(m.chat, {image: {url: haha}, caption: `_${command}_`.trim()}, {quoted: m});
   }
