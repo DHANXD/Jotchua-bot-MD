ea9c40271d8a9760b5f62e84739cbbcbbc9ad641
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2023-11-16 22:18:31 -0300

    Update buscador-stickersearch.js

--- plugins/buscador-stickersearch.js
+++ plugins/buscador-stickersearch.js
@@ -6,13 +6,13 @@ const handler = async (m, {text, usedPrefix, command, conn}) => {
   try {
     const res2 = await googleImage(text);
     const sfoto = res2.getRandom();
-    if (!text) throw `*[❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴𝙻 𝙿𝙰𝚀𝚄𝙴𝚃𝙴 𝚀𝚄𝙴 𝙳𝙴𝚂𝙴𝙴 𝙱𝚄𝚂𝙲𝙰𝚁*`;
+    if (!text) throw `*𝐈𝐍𝐆𝐑𝐄𝐒𝐄 𝐄𝐋 𝐍𝐎𝐌𝐁𝐑𝐄 𝐃𝐄𝐋 𝐏𝐀𝐐𝐔𝐄𝐓𝐄 𝐐𝐔𝐄 𝐃𝐄𝐒𝐄𝐄 𝐁𝐔𝐒𝐂𝐀𝐑*`;
     const json = await fetch(`https://api.akuari.my.id/search/sticker?query=${text}`);
     const jsons = await json.json();
-    const res = jsons.result.map((v, index) => `*🪴 • Resultado:* ${1 + index}\n*🌵 • Nombre:* ${v.title}\n*🍂 • Url:* ${v.url}`).join`\n\n───\n\n`;
+    const res = jsons.result.map((v, index) => `* • Resultado:* ${1 + index}\n* • Nombre:* ${v.title}\n* • Url:* ${v.url}`).join`\n\n───\n\n`;
     await conn.sendFile(m.chat, sfoto, 'error.jpg', res, m);
   } catch {
-    await m.reply('*[❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝚁𝙽𝚃𝙰𝚁𝙻𝙾*');
+    await m.reply('*𝐄𝐑𝐑𝐎𝐑, 𝐏𝐎𝐑𝐅𝐀𝐕𝐎𝐑 𝐕𝐔𝐄𝐋𝐕𝐀 𝐀 𝐈𝐍𝐓𝐄𝐍𝐓𝐀𝐑𝐋𝐎*');
   }
 };
 handler.tags = ['sticker', 'search'];
