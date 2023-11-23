eccdc9926d6f10a247ea825009209b4243643f97
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2023-11-23 16:23:06 -0300

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -3,8 +3,8 @@ import fetch from 'node-fetch';
 const handler = async (m, { conn, args }) => {
    const res = await fetch('https://api.github.com/repos/AleXD0009/Jotchua-bot-MD');
    const json = await res.json();
-   let txt = `           *乂  B O T  -  S C R I P T  乂*\n\n`;
-      txt += `◦  *Nombre:* ${json?.name || 'TheMystic-Bot-MD'}\n`;
+   let txt = `           *  B O T  -  S C R I P T  *\n\n`;
+      txt += `◦  *Nombre:* ${json?.name || 'Jotchua-bot-MD'}\n`;
       txt += `◦  *Visitantes:* ${json?.watchers_count || '-'}\n`;
       txt += `◦  *Tamaño:* ${(json?.size / 1024).toFixed(2) || '-'} MB\n`;
       txt += `◦  *Actualización:* ${moment(json?.updated_at).format('DD/MM/YY - HH:mm:ss') || '-'}\n`;
