9930dc757e11e7c9c6fbea1b62cbde13e8422809
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2023-11-25 06:30:11 -0300

    Update menu-audios.js

--- plugins/menu-audios.js
+++ plugins/menu-audios.js
@@ -20,61 +20,66 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
     const document = doc[Math.floor(Math.random() * doc.length)];
     const str = 
-`╭⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊
-┃  — *𝐉𝐨𝐭𝐜𝐡𝐮𝐚-𝐛𝐨𝐭-𝐌𝐃* —
-┃⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊
-┃➤ *𝗛ola, ${taguser}*
-┃⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊
-┃ © 𝐉𝐨𝐭𝐜𝐡𝐮𝐚-𝐛𝐨𝐭-𝐌𝐃 | 𝐁𝐲 𝐀𝐋𝐒
-╰⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊
+`╭───────────────────────
+│╭⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊
+│┃  — *𝐉𝐨𝐭𝐜𝐡𝐮𝐚-𝐛𝐨𝐭-𝐌𝐃* —
+│┃⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊
+│┃ *𝗛ola, ${taguser}*
+│┃⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊
+│┃ © 𝐉𝐨𝐭𝐜𝐡𝐮𝐚-𝐛𝐨𝐭-𝐌𝐃 | 𝐁𝐲 𝐀𝐋𝐒
+│╰⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊
+╰────────────────────────
 
-╭⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊
-┃ *< 𝐌𝐄𝐍𝐔 𝐀𝐔𝐃𝐈𝐎𝐒 >*
-┃⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊
-┣*- 𝙴𝚂𝙲𝚁𝙸𝙱𝙴 𝙻𝙰𝚂 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴𝚂 𝙿𝙰𝙻𝙰𝙱𝚁𝙰𝚂 𝙾 𝙵𝚁𝙰𝚂𝙴𝚂 𝚂𝙸𝙽 𝙽𝙸𝙽𝙶𝚄𝙽 𝙿𝚁𝙴𝙵𝙸𝙹𝙾 (#, /, *, .)*
-┣ ☾  _Quien es tu sempai botsito 7w7_
-┣ ☾  _Te diagnostico con gay_
-┣ ☾  _No digas eso papu_
-┣ ☾  _A nadie le importa_
-┣ ☾  _Fiesta del admin_
-┣ ☾  _Fiesta del administrador_ 
-┣ ☾  _Vivan los novios_
-┣ ☾  _Feliz cumpleaños_
-┣ ☾  _Noche de paz_
-┣ ☾  _Buenos dias_
-┣ ☾  _Buenos tardes_
-┣ ☾  _Buenos noches_
-┣ ☾  _Audio hentai_
-┣ ☾  _Chica lgante_
-┣ ☾  _Feliz navidad_
-┣ ☾  _Vete a la vrg_
-┣ ☾  _Pasa pack Bot_
-┣ ☾  _Atencion grupo_
-┣ ☾  _Marica quien_
-┣ ☾  _Murio el grupo_
-┣ ☾  _Oh me vengo_
-┣ ☾  _tio que rico_
-┣ ☾  _Viernes_
-┣ ☾  _Baneado_
-┣ ☾  _Sexo_
-┣ ☾  _Hola_
-┣ ☾  _Un pato_
-┣ ☾  _Nyanpasu_
-┣ ☾  _Te amo_
-┣ ☾  _Yamete_
-┣ ☾  _Bañate_
-┣ ☾  _Es puto_
-┣ ☾  _La biblia_
-┣ ☾  _Onichan_
-┣ ☾  _Mierda de Bot_
-┣ ☾  _Siuuu_
-┣ ☾  _Epico_
-┣ ☾  _Shitpost_
-┣ ☾  _Rawr_
-┣ ☾  _UwU_
-┣ ☾  _:c_
-┣ ☾  _a_
-╰⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊`.trim();
+╭───────────────────────
+│╭⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊
+│┃ *< 𝐌𝐄𝐍𝐔 𝐀𝐔𝐃𝐈𝐎𝐒 >*
+│┃⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊
+│┣*- 𝙴𝚂𝙲𝚁𝙸𝙱𝙴 𝙻𝙰𝚂 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴𝚂 𝙿𝙰𝙻𝙰𝙱𝚁𝙰𝚂 𝙾 𝙵𝚁𝙰𝚂𝙴𝚂 𝚂𝙸𝙽 𝙽𝙸𝙽𝙶𝚄𝙽 𝙿𝚁𝙴𝙵𝙸𝙹𝙾 (#, /, *, .)*
+│┣ ☾  _Quien es tu sempai botsito 7w7_
+│┣ ☾  _Te diagnostico con gay_
+│┣ ☾  _No digas eso papu_
+│┣ ☾  _A nadie le importa_
+│┣ ☾  _Fiesta del admin_
+│┣ ☾  _Fiesta del administrador_ 
+│┣ ☾  _Vivan los novios_
+│┣ ☾  _Feliz cumpleaños_
+│┣ ☾  _Noche de paz_
+│┣ ☾  _Buenos dias_
+│┣ ☾  _Buenos tardes_
+│┣ ☾  _Buenos noches_
+│┣ ☾  _Audio hentai_
+│┣ ☾  _Chica lgante_
+│┣ ☾  _Feliz navidad_
+│┣ ☾  _Vete a la vrg_
+│┣ ☾  _Pasa pack Bot_
+│┣ ☾  _Atencion grupo_
+│┣ ☾  _Marica quien_
+│┣ ☾  _Murio el grupo_
+│┣ ☾  _Oh me vengo_
+│┣ ☾  _tio que rico_
+│┣ ☾  _Viernes_
+│┣ ☾  _Baneado_
+│┣ ☾  _Sexo_
+│┣ ☾  _Hola_
+│┣ ☾  _Un pato_
+│┣ ☾  _Nyanpasu_
+│┣ ☾  _Te amo_
+│┣ ☾  _Yamete_
+│┣ ☾  _Bañate_
+│┣ ☾  _Es puto_
+│┣ ☾  _La biblia_
+│┣ ☾  _Onichan_
+│┣ ☾  _Mierda de Bot_
+│┣ ☾  _Siuuu_
+│┣ ☾  _Epico_
+│┣ ☾  _Shitpost_
+│┣ ☾  _Rawr_
+│┣ ☾  _UwU_
+│┣ ☾  _:c_
+│┣ ☾  _a_
+│╰⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊⚊
+╰────────────────────────
+`.trim();
     if (m.isGroup) {
       // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
       const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
@@ -85,7 +90,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
       conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
     }
   } catch {
-    conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙼𝙴𝙽𝚄 𝚃𝙸𝙴𝙽𝙴 𝚄𝙽 𝙴𝚁𝚁𝙾𝚁 𝚈 𝙽𝙾 𝙵𝚄𝙴 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝙴𝙽𝚅𝙸𝙰𝚁𝙻𝙾, 𝚁𝙴𝙿𝙾𝚁𝚃𝙴𝙻𝙾 𝙰𝙻 𝙿𝚁𝙾𝙿𝙸𝙴𝚃𝙰𝚁𝙸𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃*', m);
+    conn.reply(m.chat, '*_𝐄𝐋 𝐌𝐄𝐍𝐔 𝐓𝐈𝐄𝐍𝐄 𝐔𝐍 𝐄𝐑𝐑𝐎𝐑 𝐘 𝐍𝐎 𝐄𝐒 𝐏𝐎𝐒𝐈𝐁𝐋𝐄 𝐄𝐍𝐕𝐈𝐀𝐑𝐋𝐎, 𝐑𝐄𝐏𝐎𝐑𝐓𝐄𝐋𝐎 𝐀𝐋 𝐏𝐑𝐎𝐏𝐈𝐄𝐓𝐀𝐑𝐈𝐎 𝐃𝐄𝐋 𝐁𝐎𝐓_*', m);
   }
 };
 handler.command = /^(menu2|audios|menú2|memu2|menuaudio|menuaudios|memuaudios|memuaudio|audios|keyaudio|keyaudios)$/i;
